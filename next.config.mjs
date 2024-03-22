86a832a1ff396a8e5a093760c52988f30e12885e
Jagadheesh01 <jagdheesh.n@makoitlab.com>
2024-03-22 18:23:24 +0530

    commit after adding mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Add alias for client-side code
+    config.resolve.alias['@'] = path.resolve(__dirname, './src');
+
+    // Add alias for server-side code if needed
+    if (isServer) {
+      // config.resolve.alias['@server'] = path.resolve(__dirname, 'server');
+      // Add server-side aliases if necessary
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
